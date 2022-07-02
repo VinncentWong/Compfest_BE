7a3c5cb3b80fca711685a373cda3613318a1a7c9
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-02 17:02:59 +0700

    add cors configuration

--- src/main/java/com/demo/controller/UserController.java
+++ src/main/java/com/demo/controller/UserController.java
@@ -4,10 +4,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.demo.dto.UserDto;
@@ -15,6 +17,7 @@ import com.demo.service.UserService;
 import com.demo.util.AppResponse;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:3000"}, allowedHeaders = {"*"}, methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.PATCH, RequestMethod.POST, RequestMethod.PUT})
 public class UserController {
 	
 	@Autowired
--- src/main/java/com/demo/security/SecurityConfig.java
+++ src/main/java/com/demo/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.demo.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.demo.security.filter.CustomUsernamePasswordFilter;
 import com.demo.security.filter.JwtFilter;
@@ -26,19 +31,47 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-		http.addFilterAt(new CustomUsernamePasswordFilter(), UsernamePasswordAuthenticationFilter.class);
-		http.addFilterAfter(new JwtFilter(), CustomUsernamePasswordFilter.class);
-		http.cors(cors -> {
-			CorsConfigurationSource source = (request) -> {
-				CorsConfiguration configuration = new CorsConfiguration();
-				configuration.addAllowedOrigin("http://127.0.0.1:3000");
-				configuration.addAllowedMethod("*");
-				configuration.addAllowedHeader("*");
-				return configuration;
-			};
-		});
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT","OPTIONS","PATCH", "DELETE"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        
+		http
+		.authorizeRequests()
+		.anyRequest()
+		.permitAll()
+		.and()
+		.addFilterAt(new CustomUsernamePasswordFilter(), UsernamePasswordAuthenticationFilter.class)
+		.addFilterAfter(new JwtFilter(), CustomUsernamePasswordFilter.class)
+		.csrf().disable()
+		.cors().configurationSource(request -> corsConfiguration);
+//		.cors(cors -> {
+//			CorsConfigurationSource source = (request) -> {
+//				CorsConfiguration configuration = new CorsConfiguration();
+//				configuration.setAllowCredentials(true);
+//				configuration.setAllowedOrigins(List.of("http://192.168.1.20:3000"));
+//				configuration.setAllowedHeaders(List.of("*"));
+//				configuration.setAllowedMethods(List.of("*"));
+//				configuration.setExposedHeaders(List.of("Access-Control-Allow-Origin", "Access-Control-Request-Headers", "Access-Control-Allow-Credentials", "Access-Control-Request-Method"));
+//				return configuration;
+//			};
+//			cors.configurationSource(source);
+//		});
 	}
 	
-	
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("*"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
+		
+	}
 }
