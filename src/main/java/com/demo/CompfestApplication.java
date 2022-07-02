46ab6d1e05156cd59d27325f6abda8497177c113
Vinncent Alexander Wong <vinncentwong2@gmail.com>
2022-07-02 10:07:44 +0700

    initial config

---
+++ src/main/java/com/demo/CompfestApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompfestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompfestApplication.class, args);
+	}
+
+}
